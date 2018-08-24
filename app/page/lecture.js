39935d01d304637ba79073b528b3552971d8ae0f
snutiise <snutiise@hanmail.net>
2018-08-25 00:47:14 +0900

    강의 목록 초기 작업

--- app/page/lecture.js
+++ app/page/lecture.js
@@ -8,7 +8,7 @@ import * as cookie from '../util/cookie';
 export default class lecture extends Component {
     constructor(props) {
         super(props);
-        this.state = {isLogin:cookie.getCookie('user'), type:''};
+        this.state = {isLogin:cookie.getCookie('user'), type:'', list:[]};
         let tmp = this;
         service.getType().then(function (res) {
             tmp.setState({ type: res.data });
@@ -17,7 +17,9 @@ export default class lecture extends Component {
         });
     }
     componentDidMount(){
+        const curosr =this;
         service.lectureList().then(function (res) {
+            curosr.setState({list:res.data});
             console.log(res.data);
         }).catch(function (error) {
             alert('error massage : '+error);
@@ -26,6 +28,17 @@ export default class lecture extends Component {
     render() {
         return (
             <div className={style.wrapper}>
+                <div className={style.lectureWrapper}>
+                {this.state.list.map((lecture, i) => {
+                        return (<div className={style.lectureBox} key={i}>
+                                    <img src="../resources/img/main.png"/>
+                                    <div className={style.lectureTitle}>{lecture.title}</div>
+                                    <div className={style.lectureName}>{lecture.name}</div>
+                                    <div className={style.lectureSchedule}>{lecture.schedule}</div>
+                                    <div className={style.lecturePrice}>{lecture.price}</div>
+                                 </div>);
+                })}
+                </div>
                 { this.state.type=="강사" ?<div className={style.register}><div className={style.textWrapper}><Link to="/lectureReg">과외등록</Link></div></div>:null}
             </div>
         );
