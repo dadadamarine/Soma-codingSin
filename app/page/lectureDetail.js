669383f5297c115de51a2da6993a7978838e21a6
snutiise <snutiise@hanmail.net>
2018-08-25 02:22:38 +0900

    data 적용

--- app/page/lectureDetail.js
+++ app/page/lectureDetail.js
@@ -16,7 +16,7 @@ export default class lectureDtail extends Component {
             alert('error massage : '+error);
         });
         service.lecture(this.state.id).then(function (res) {
-            curosr.setState({ lecture: res.data });
+            curosr.setState({ lecture: res.data[0] });
         }).catch(function (error) {
             alert('error massage : '+error);
         });
@@ -45,20 +45,20 @@ export default class lectureDtail extends Component {
                     <div className={style.profile}>
                         <img src="../resources/img/profile.png" />
                         <div className={style.profileTextWrapper}>
-                            <div className={style.profileText}>전승훈</div>
-                            <div className={style.profileText}>ss9305@hanmail.net</div>
+                            <div className={style.profileText}>{this.state.lecture.name}</div>
+                            <div className={style.profileText}>{this.state.lecture.email}</div>
                             <div className={style.profileText}>약력 및 코멘트 작성</div>
                         </div>
                     </div>
                     <br/><br/><br/><br/>
-                    <div className={style.title}>0부터 시작하는 웹프론트 강의!</div>
-                    <div className={style.lectureText}>0부터 시작하는 웹프론트 강의!</div>
+                    <div className={style.title}>{this.state.lecture.title}</div>
+                    <div className={style.lectureText}>{this.state.lecture.description}</div>
                     <br/><br/>
                     <div className={style.title}>강의시간</div>
-                    <div className={style.lectureText}>0부터 시작하는 웹프론트 강의!</div>
+                    <div className={style.lectureText}>{this.state.lecture.schedule}</div>
                     <br/><br/>
                     <div className={style.title}>가격</div>
-                    <div className={style.lectureText}>250,000</div>
+                    <div className={style.lectureText}>{this.state.lecture.price}</div>
                     <br/><br/>
                     { this.state.type=="학생" ?<Button secondary fluid content='과외신청' onClick={this.handleSubmit}/>:null}
                 </div>
