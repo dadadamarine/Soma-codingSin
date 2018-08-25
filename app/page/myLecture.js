e2c3e7664c0be49c47604eed726bec6c4e412d5f
snutiise <snutiise@hanmail.net>
2018-08-25 14:58:36 +0900

    null is not

--- app/page/myLecture.js
+++ app/page/myLecture.js
@@ -8,7 +8,7 @@ import * as cookie from '../util/cookie';
 export default class myLecture extends Component {
     constructor(props) {
         super(props);
-        this.state = {isLogin:cookie.getCookie('user'), list:null};
+        this.state = {isLogin:cookie.getCookie('user'), list:[]};
         const curosr = this;
         service.myLectureList().then(function (res) {
             console.log(res.data);
@@ -23,7 +23,7 @@ export default class myLecture extends Component {
         return (
             <div className={style.wrapper}>
                 <div className={style.lectureWrapper}>
-                {list!=null?this.state.list.map((lecture, i) => {
+                {list!=[]?this.state.list.map((lecture, i) => {
                     let url = "/room#"+lecture._id;
                         return (<Link to={url}><div className={style.lectureBox} key={i}>
                                     <img src="../resources/img/main.png"/>
