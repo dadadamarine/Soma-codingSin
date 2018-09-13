36b677dd2413cbe97446cb188246f7fe15d72f6a
snutiise <snutiise@hanmail.net>
2018-09-13 14:15:05 +0900

    강의 스케줄 달력 추가

--- app/page/lectureReg.js
+++ app/page/lectureReg.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import * as service from '../request/lecture';
 import { Segment, Input, Button, Divider } from 'semantic-ui-react';
 import style from './lectureReg.css';
+import Picker from '../util/picker';
 
 export default class lectureReg extends Component {
     constructor(props) {
         super(props);
-        this.state = {title:'', description:'', schedule:'', price:''};
+        this.state = {title:'', 
+        description:'', 
+        schedule:'', 
+        price:'',    
+        startValue: null,
+        endValue: null,
+        startOpen: false,
+        endOpen: false,};
      
         this.inputTITLE = this.inputTITLE.bind(this);
         this.inputDESCRIPTION = this.inputDESCRIPTION.bind(this);
         this.inputSCHEDULE = this.inputSCHEDULE.bind(this);
         this.inputPRICE = this.inputPRICE.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onStartOpenChange = this.onStartOpenChange.bind(this);
+        this.onEndOpenChange = this.onEndOpenChange.bind(this);
+        this.onStartChange = this.onStartChange(this);
+        this.onEndChange = this.onEndChange(this);
+        this.disabledStartDate = this.disabledStartDate.bind(this);
     }
     inputTITLE(event) {
         this.setState({ title: event.target.value });
@@ -26,6 +39,39 @@ export default class lectureReg extends Component {
     inputPRICE(event) {
         this.setState({ price: event.target.value });
     }
+    onStartOpenChange(event){
+        this.setState({
+            startOpen: event.target.open
+        });
+    }
+    onEndOpenChange(event){
+        this.setState({
+            endOpen : event.target.open
+        });
+    }
+    onStartChange(event){
+        this.setState({
+            startValue: event.target.showValue,
+            startOpen: false,
+            endOpen: true,
+        });
+    }
+    onEndChange(event){
+        this.setState({
+            endValue: event.target.showValue,
+        });
+    }
+    disabledStartDate(event){
+        if (!event.target.showValue) {
+            return false;
+        }
+        const startValue = this.state.startValue;
+        if (!startValue) {
+            return false;
+        }
+        return event.target.showValue.diff(startValue, 'days') < 0;
+    }
+
     handleSubmit(event) {
         const curosr =this;
         service.lectureRegister(this.state.title, this.state.description, this.state.schedule, this.state.price).then(function (res) {
@@ -40,6 +86,7 @@ export default class lectureReg extends Component {
         event.preventDefault();
     }
     render() {
+        const state = this.state;
         return (
             <div className={style.wrapper}>
                 <div className={style.lectureBox}>
@@ -48,7 +95,26 @@ export default class lectureReg extends Component {
                         <br /><br />
                         <textarea className={style.despBox} placeholder='강의설명' value={this.state.description} onChange={this.inputDESCRIPTION} />
                         <br /><br />
-                        <Input type="text" placeholder='강의시간' value={this.state.schedule} onChange={this.inputSCHEDULE} />
+                        <div style={{ width: 240, margin: 20 }}>
+                        시작일：
+                        <Picker
+                            onOpenChange={this.onStartOpenChange}
+                            type="start"
+                            showValue={state.startValue}
+                            open={this.state.startOpen}
+                            value={[state.startValue, state.endValue]}
+                            onChange={this.onStartChange}/>
+                        종료일：
+                        <Picker
+                            onOpenChange={this.onEndOpenChange}
+                            open={this.state.endOpen}
+                            type="end"
+                            showValue={state.endValue}
+                            disabledDate={this.disabledStartDate}
+                            value={[state.startValue, state.endValue]}
+                            onChange={this.onEndChange}
+                        />
+                        </div>
                         <br /><br />
                         <Input type="text" placeholder='가격' value={this.state.price} onChange={this.inputPRICE} />
                         <br /><br />
---
+++ app/util/picker.js
@@ -0,0 +1,74 @@
+
+import 'rc-calendar/assets/index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RangeCalendar from 'rc-calendar/lib/RangeCalendar';
+import DatePicker from 'rc-calendar/lib/Picker';
+
+import koKR from 'rc-calendar/lib/locale/ko_KR';
+import enUS from 'rc-calendar/lib/locale/en_US';
+
+import moment from 'moment';
+import 'moment/locale/ko';
+import 'moment/locale/en-gb';
+
+const format = 'YYYY-MM-DD';
+
+const fullFormat = 'YYYY-MM-DD dddd';
+const ko = true;
+
+const now = moment();
+if (ko) {
+  now.locale('ko').utcOffset(9);
+} else {
+  now.locale('en-gb').utcOffset(0);
+}
+
+class Picker extends React.Component {
+  state = {
+    hoverValue: [],
+  };
+
+  onHoverChange = (hoverValue) => {
+    console.log(hoverValue);
+    this.setState({ hoverValue });
+  }
+
+  render() {
+    const props = this.props;
+    const { showValue } = props;
+    const calendar = (
+      <RangeCalendar
+        hoverValue={this.state.hoverValue}
+        onHoverChange={this.onHoverChange}
+        type={this.props.type}
+        locale={ko ? koKR : enUS}
+        defaultValue={now}
+        format={format}
+        onChange={props.onChange}
+        disabledDate={props.disabledDate}
+      />);
+    return (
+      <DatePicker
+        open={this.props.open}
+        onOpenChange={this.props.onOpenChange}
+        calendar={calendar}
+        value={props.value}
+      >
+        {
+          () => {
+            return (
+              <span>
+                <input
+                  placeholder="강의 기간"
+                  style={{ width: 200 }}
+                  readOnly
+                  value={showValue && showValue.format(fullFormat) || ''}
+                />
+                </span>
+            );
+          }
+        }
+      </DatePicker>);
+  }
+}
