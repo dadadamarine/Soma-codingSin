71aadd53f8ce6a867702eb48c65bb29a1d5d3328
snutiise <snutiise@hanmail.net>
2018-09-22 00:36:59 +0900

    ...

--- app/request/admin.js
+++ app/request/admin.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export function contentWrite(u_type, u_title, u_content, u_chapter) {
     return axios.post('/admin/contentWrite', {
-        id: u_type,
-        pw: u_title,
-        name: u_content,
-        email: u_chapter
+        type: u_type,
+        title: u_title,
+        content: u_content,
+        chapter: u_chapter
     });
 }
