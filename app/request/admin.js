7e10545ffdfe2404d4841d9897118ecd4037db85
snutiise <snutiise@hanmail.net>
2018-09-22 00:32:35 +0900

    이게 왜 들어가있어 ㅡㅡ

--- app/request/admin.js
+++ app/request/admin.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export function contentWrite(u_type, u_title, u_content, u_chapter) {
-    const hash = CryptoJS.SHA256(u_pw); 
     return axios.post('/admin/contentWrite', {
         id: u_type,
         pw: u_title,
