6816bcc9c61f3ea612bd73dc1c211bc1daa5f695
snutiise <snutiise@hanmail.net>
2018-10-06 02:56:24 +0900

    ide

--- app/util/ide.js
+++ app/util/ide.js
@@ -1,69 +1,136 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Codemirror = require('react-codemirror');
-const createReactClass = require('create-react-class');
-var style= require('./ide.css');
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var Codemirror = require('react-codemirror');
+// const createReactClass = require('create-react-class');
+// var style= require('./ide.css');
+// var io = require('socket.io-client');
+// const socket = io.connect({ reconnect: true });
+
+// require('codemirror/mode/javascript/javascript');
+// require('codemirror/mode/xml/xml');
+// // require('codemirror/mode/markdown/markdown');
+// require('codemirror/mode/clike/clike');
+
+// var defaults = {
+// 	// markdown: '# 코딩의 신에 오신걸 환영합니다. \n',
+//     javascript: '# 코딩의 신에 오신걸 환영합니다. \n',
+//     clike: '# 코딩의 신에 오신걸 환영합니다. \n'
+// };
+
+// var Ide = createReactClass({
+// 	componentDidMount() {
+// 		var cursor=this;
+// 		// var hashString = location.hash.replace('#', '');
+// 		socket.emit('channelJoin', "test");
+// 		socket.on('receive', function (data) {
+// 			cursor.setState({code_y:data});
+// 		});
+// 	},
+// 	getInitialState () {
+// 		return {
+// 			code_i: defaults.javascript,
+// 			mode_i: 'javascript'
+// 		};
+// 	},
+// 	updateCode (newCode) {
+// 		this.setState({
+// 			code_i: newCode
+// 		});
+// 		socket.emit('send', {channel:"test", msg:newCode});
+// 	},
+// 	changeMode (e) {
+//         var mode = e.target.value;
+// 		this.setState({
+// 			mode_i: mode,
+// 			code_i: defaults[mode]
+// 		});
+// 	},
+// 	render () {
+// 		var options_i = {
+// 			lineNumbers: true,
+// 			readOnly: false,
+// 			mode: this.state.mode_i
+// 		};
+// 		var options_y = {
+// 			lineNumbers: true,
+// 			readOnly: true,
+// 			mode: this.state.mode_y
+// 		};
+// 		return (
+// 			<div className={style.flexRow}>
+// 				<div className={style.flexColumn}>
+// 				<Codemirror ref="editor" value={this.state.code_i} onChange={this.updateCode} options={options_i} autoFocus={true} />
+// 				<div style={{ marginTop: 10 }}>
+// 					<select onChange={this.changeMode} value={this.state.mode_i}>
+// 						{/* <option value="markdown">Markdown</option> */}
+// 						<option value="javascript">JavaScript</option>
+//                         <option value="clike">C</option>
+// 					</select>
+// 				</div>
+// 				</div>
+// 				<div className={style.margin} />
+// 				<div className={style.flexColumn}>
+// 				<Codemirror ref="editor" value={this.state.code_y} options={options_y}/>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// export default Ide;
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+import style from './ide.css';
+require('codemirror/mode/javascript/javascript');
 var io = require('socket.io-client');
 const socket = io.connect({ reconnect: true });
 
-require('codemirror/mode/javascript/javascript');
-require('codemirror/mode/xml/xml');
-// require('codemirror/mode/markdown/markdown');
-require('codemirror/mode/clike/clike');
-
-var defaults = {
-	// markdown: '# 코딩의 신에 오신걸 환영합니다. \n',
-    javascript: '# 코딩의 신에 오신걸 환영합니다. \n',
-    clike: '# 코딩의 신에 오신걸 환영합니다. \n'
-};
-
-var Ide = createReactClass({
-	componentDidMount() {
-		console.log(this.props);
+export default class room extends Component {
+	constructor(props) {
+		super();
+		this.state = {code_i:"",code_y:"", mode_i:"javascript",mode_y:"javascript", mode_intro:{javascript: '# 코딩의 신에 오신걸 환영합니다. \n',clike: '// 코딩의 신에 오신걸 환영합니다. \n'}};
 		var cursor=this;
 		// var hashString = location.hash.replace('#', '');
 		socket.emit('channelJoin', "test");
 		socket.on('receive', function (data) {
 			cursor.setState({code_y:data});
-			console.log(data);
 		});
-	},
-	getInitialState () {
-		return {
-			code_i: defaults.javascript,
-			mode_i: 'javascript'
-		};
-	},
-	updateCode (newCode) {
-		this.setState({
-			code_i: newCode
-		});
-		socket.emit('send', {channel:"test", msg:newCode});
-	},
+		this.changeMode = this.changeMode.bind(this);
+		this.setState({code_i:this.state.mode_intro.javascript});
+		this.instance = null;
+	}
+	componentDidMount() {
+	}
 	changeMode (e) {
-        var mode = e.target.value;
+		let mode = e.target.value;
+		let intro = this.state.mode_intro[mode];
+		alert(intro);
 		this.setState({
 			mode_i: mode,
-			code_i: defaults[mode]
+			code_i: intro
 		});
-	},
-	render () {
-		var options_i = {
-			lineNumbers: true,
-			readOnly: false,
-			mode: this.state.mode_i
-		};
-		var options_y = {
-			lineNumbers: true,
-			readOnly: true,
-			mode: this.state.mode_y
-		};
+	}
+	render() {
 		return (
 			<div className={style.flexRow}>
 				<div className={style.flexColumn}>
-				<Codemirror ref="editor" value={this.state.code_i} onChange={this.updateCode} options={options_i} autoFocus={true} />
+				<CodeMirror
+					value={this.state.code_i}
+					options={{
+						mode: `${this.state.mode_i}`,
+						theme: 'material',
+						lineNumbers: true
+					}}
+					onChange={(editor, data, value) => {
+						socket.emit('send', {channel:"test", msg:value});
+					}}
+					editorDidMount={editor => { editor.setValue(this.state.mode_intro.javascript);}}
+					autoCursor={true}
+				/>
 				<div style={{ marginTop: 10 }}>
-					<select onChange={this.changeMode} value={this.state.mode_i}>
+					<select onChange={this.changeMode}>
 						{/* <option value="markdown">Markdown</option> */}
 						<option value="javascript">JavaScript</option>
                         <option value="clike">C</option>
@@ -72,11 +139,18 @@ var Ide = createReactClass({
 				</div>
 				<div className={style.margin} />
 				<div className={style.flexColumn}>
-				<Codemirror ref="editor" value={this.state.code_y} options={options_y}/>
+				<CodeMirror
+				value={this.state.code_y}
+				options={{
+					mode: `${this.state.mode_y}`,
+					theme: 'material',
+					lineNumbers: true
+				}}
+				editorDidMount={editor => { this.instance = editor }}
+				autoCursor={true}
+			/>
 				</div>
 			</div>
-		);
+		)
 	}
-});
-
-export default Ide;
+}
