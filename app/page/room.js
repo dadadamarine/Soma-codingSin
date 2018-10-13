6802ea1008ab43ed3ea0a4bf3958b46bab335ab8
snutiise <snutiise@hanmail.net>
2018-10-13 15:34:29 +0900

    error test

--- app/page/room.js
+++ app/page/room.js
@@ -44,12 +44,12 @@ export default class room extends Component {
     for(let a=0;a<list_tmp.length;a++){
         let answer_tmp2 = new Array();
         let text = list_tmp[a].content.split('\n');
-        
-        console.log(list_tmp[a].quiz);
-        Console.log(String(list_tmp[a].quiz));
+
+        console.log(list_tmp[a].quiz.length);
+        console.log(String(list_tmp[a].quiz));
         let quiz_list = String(list_tmp[a].quiz).split(",");
-        for(let i=0;i<quiz_list.length;i++){
-            let tmp = String(quiz_list[i]).split(",");
+        for(let i=0;i<list_tmp[a].quiz.length;i++){
+            let tmp = String(list_tmp[a].quiz[i]).split(",");
             let str = String(text[tmp[0]]).substring(tmp[1], Number(tmp[2])+1);
             answer_tmp2.push(str);
             text[tmp[0]]=String(text[tmp[0]]).replace(str,"{quiz}");
