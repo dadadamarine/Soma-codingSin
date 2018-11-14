54b73fe33bda3974272f492276a962165a35c3a3
dadadamarine <dadadamarine@naver.com>
2018-11-15 00:02:16 +0900

    공지사항 페이지 파일 생성

--- app/page/tutorReview.js
+++ app/page/tutorReview.js
@@ -43,55 +43,37 @@ class tutorDetail extends Component{
                     <div className={[style.sizeWrapper, style.flex].join(" ")}>
                         <div className={style["arrow--left"]}></div>
                         <div className={style["review-box"]}>
-                            <div>
-                                <h2 className={style["review-box__title"]}>매우 친절한 강의!</h2>
+                            
+
+                            <div className={[style["review-box__img"], style["review-box__img--2"]].join(" ")} >
+                                <h2 className={style["review-box__name"]}>수강생 정O석</h2>
                             </div>
 
-                            <img className={style["review-box__img"]} src={require("../resources/img/tutorDetail/review/review1.png")} alt=""/>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, illo cum quisquam placeat sapiente, sit minima iure ten</p>
+                            <p className={style["review-box__text"]}>내가 제일 좋아하는 강의는 원예나선생님의 문장확장편이다! 문장확장편 수업을 들으면서 길고 다양한 문장을 구사할수 있도록 연습할 수 있어서 너무 좋습니다ㅎㅎ 복습편으로 준비된 문장을 통해서 다양한 문장을 공부할 수있는점도 좋습</p>
 
                                 
                         </div>
                         <div className={style["review-box"]}>
-                            <div>
-                                <h2 className={style["review-box__title"]}>매우 친절한 강의에요!</h2>
-                            </div>
-                            <ul>
-                                <li>한국건설기술연구원 방내화시험설비제어시스템 개111발 참여</li>
-                                <li>(주)폴리스푸드(Malaysua) 속도연동지능화 프로젝트 개발 참여</li>    
-                            </ul>
                             
-                            <div>
-                                <div className={style.verticalBar}></div>
-                                <p>경력사항</p>
-                            </div>
-                            <ul>
-                                <li>서울대 윤리교육과 대학원</li>
-                                <li>현 {"<대학생 철학교실>"} 강사</li>    
-                                <li>전 김영준 국어논술 학원 전임강사</li>    
 
-                            </ul>
+                            <div className={[style["review-box__img"], style["review-box__img--1"]].join(" ")} >
+                                <h2 className={style["review-box__name"]}>수강생 정O석</h2>
+                            </div>
+
+                            <p className={style["review-box__text"]}>문득 영어를 너무 놓고 있었던 제가 한심하게 느껴져서 알아보던 중에 공부를 하게 되었습니다. 아직 한달차입니다. 하지만 하루도 빠짐 없이 시간 내서 강의듣고 복습하려고 노력하고 있습니다~ 권필쌤 강의보면서 재미있게 영어 배우고 있 </p>
+
                                 
                         </div>
-                         <div className={style["review-box"]}>
-                            <div>
-                                <h2 className={style["review-box__title"]}>매우 친절한 강의에요!</h2>
-                            </div>
-                            <ul>
-                                <li>한국건설기술연구원 방내화시험설비제어시스템 개111발 참여</li>
-                                <li>(주)폴리스푸드(Malaysua) 속도연동지능화 프로젝트 개발 참여</li>    
-                            </ul>
-                            
-                            <div>
-                                <div className={style.verticalBar}></div>
-                                <p>경력사항</p>
-                            </div>
-                            <ul>
-                                <li>서울대 윤리교육과 대학원</li>
-                                <li>현 {"<대학생 철학교실>"} 강사</li>    
-                                <li>전 김영준 국어논술 학원 전임강사</li>    
 
-                            </ul>
+                            <div className={style["review-box"]}>
+                            
+
+                            <div className={[style["review-box__img"], style["review-box__img--3"]].join(' ')} >
+                                <h2 className={style["review-box__name"]}>수강생 정O석</h2>
+                            </div>
+
+                            <p className={style["review-box__text"]}>상황에 맞는 표현의 대화뿐만아니라 실제 원어민들이 말하는 문장으로 발음 연습을 할수 있어서 많은 도움이돼요! 따라하다보니 말하기뿐만아니라 연음들을 듣는 능력도 느는것 같아 뿌듯합니다^^ 외국에 나가려면 돈이 많이 들어서 늘 외 </p>
+
                                 
                         </div>
                         <div className={style["arrow--right"]}></div>
